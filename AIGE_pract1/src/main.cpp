57430d8ff2a7bf4da56490d343f82d7e1644d7db
ta1lw1nd <ta1lw1nd.contact.ua@gmail.com>
2026-09-25 21:52:37 +0300

    Task 4 polished

--- AIGE_pract1/src/main.cpp
+++ AIGE_pract1/src/main.cpp
@@ -31,79 +31,79 @@ namespace Engine {
     };
 
     class LogManager {
-    private:
-        std::vector<LogElement> log;
-        float logDuration;
-        int fontSize;
-        Color logColor;
+        private:
+            std::vector<LogElement> log;
+            float logDuration;
+            int fontSize;
+            Color logColor;
 
-    public:
-        LogManager(float duration = 3.0f, int size = 18, Color color = WHITE)
-            : logDuration(duration), fontSize(size), logColor(color) {}
+        public:
+            LogManager(float duration = 3.0f, int size = 18, Color color = WHITE)
+                : logDuration(duration), fontSize(size), logColor(color) {}
 
-        void AddLog(const std::string& message) {
-            log.push_back({ message, logDuration });
-        }
-
-        void Update(float deltaTime) {
-            for (size_t i = 0; i < log.size();) {
-                log[i].timeRemaining -= deltaTime;
-                if (log[i].timeRemaining <= 0.0f) {
-                    log.erase(log.begin() + i);
-                } else {
-                    ++i;
-                }
+            void AddLog(const std::string& message) {
+                log.push_back({ message, logDuration });
             }
-        }
 
-        void Draw(float startX, float startY, const std::string& title = "EVENT LOG") {
-            if (log.empty()) return;
-
-            float padding = 12.0f;
-            float lineSpacing = 6.0f;
-            float titleFontSize = fontSize + 4.0f;
-            float headerHeight = titleFontSize + 10.0f;
-
-            float maxTextWidth = MeasureText(title.c_str(), titleFontSize);
-
-            for (const auto& elem : log) {
-                float textWidth = MeasureText(elem.message.c_str(), fontSize);
-                if (textWidth > maxTextWidth) {
-                    maxTextWidth = textWidth;
+            void Update(float deltaTime) {
+                for (size_t i = 0; i < log.size();) {
+                    log[i].timeRemaining -= deltaTime;
+                    if (log[i].timeRemaining <= 0.0f) {
+                        log.erase(log.begin() + i);
+                    } else {
+                        ++i;
+                    }
                 }
             }
 
-            float panelWidth = maxTextWidth + (padding * 2.0f);
-            float lineHeight = fontSize + lineSpacing;
-            float panelHeight = padding + headerHeight + (log.size() * lineHeight) + padding;
+            void Draw(float startX, float startY, const std::string& title = "EVENT LOG") {
+                if (log.empty()) return;
 
-            Rectangle panelRect = { startX, startY, panelWidth, panelHeight };
+                float padding = 12.0f;
+                float lineSpacing = 6.0f;
+                float titleFontSize = fontSize + 4.0f;
+                float headerHeight = titleFontSize + 10.0f;
 
-            DrawRectangleRec(panelRect, ColorAlpha(DARKGRAY, 0.85f));
-            DrawRectangle((int)startX, (int)startY, (int)panelWidth, (int)(headerHeight + padding / 2), ColorAlpha(BLACK, 0.4f));
-            DrawRectangleLinesEx(panelRect, 2.0f, ColorAlpha(WHITE, 0.8f));
+                float maxTextWidth = MeasureText(title.c_str(), titleFontSize);
 
-            DrawText(title.c_str(), (int)(startX + padding), (int)(startY + padding / 2), (int)titleFontSize, WHITE);
+                for (const auto& elem : log) {
+                    float textWidth = MeasureText(elem.message.c_str(), fontSize);
+                    if (textWidth > maxTextWidth) {
+                        maxTextWidth = textWidth;
+                    }
+                }
 
-            float lineY = startY + headerHeight + 2.0f;
-            DrawLineEx({ startX + padding / 2, lineY }, { startX + panelWidth - padding / 2, lineY }, 1.5f, ColorAlpha(WHITE, 0.5f));
+                float panelWidth = maxTextWidth + (padding * 2.0f);
+                float lineHeight = fontSize + lineSpacing;
+                float panelHeight = padding + headerHeight + (log.size() * lineHeight) + padding;
 
-            float currentY = lineY + padding;
-            for (size_t i = 0; i < log.size(); ++i) {
-                float alpha = log[i].timeRemaining / logDuration;
-                if (alpha > 1.0f) alpha = 1.0f;
+                Rectangle panelRect = { startX, startY, panelWidth, panelHeight };
 
-                Color textColor = ColorAlpha(logColor, alpha);
+                DrawRectangleRec(panelRect, ColorAlpha(DARKGRAY, 0.85f));
+                DrawRectangle((int)startX, (int)startY, (int)panelWidth, (int)(headerHeight + padding / 2), ColorAlpha(BLACK, 0.4f));
+                DrawRectangleLinesEx(panelRect, 2.0f, ColorAlpha(WHITE, 0.8f));
 
-                DrawText(log[i].message.c_str(), (int)(startX + padding), (int)currentY, fontSize, textColor);
+                DrawText(title.c_str(), (int)(startX + padding), (int)(startY + padding / 2), (int)titleFontSize, WHITE);
 
-                currentY += lineHeight;
+                float lineY = startY + headerHeight + 2.0f;
+                DrawLineEx({ startX + padding / 2, lineY }, { startX + panelWidth - padding / 2, lineY }, 1.5f, ColorAlpha(WHITE, 0.5f));
+
+                float currentY = lineY + padding;
+                for (size_t i = 0; i < log.size(); ++i) {
+                    float alpha = log[i].timeRemaining / logDuration;
+                    if (alpha > 1.0f) alpha = 1.0f;
+
+                    Color textColor = ColorAlpha(logColor, alpha);
+
+                    DrawText(log[i].message.c_str(), (int)(startX + padding), (int)currentY, fontSize, textColor);
+
+                    currentY += lineHeight;
+                }
             }
-        }
 
-        void Clear() {
-            log.clear();
-        }
+            void Clear() {
+                log.clear();
+            }
     };
 
     struct Transform {
